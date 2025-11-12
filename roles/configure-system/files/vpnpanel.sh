b7df725b91b34b0da6e712caa5d447de915a98ce
Ensar Gök <116498667+ensargx@users.noreply.github.com>
2025-11-12 22:24:04 +0300

    panel

---
+++ roles/configure-system/files/vpnpanel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+htbip=$(ip addr | grep tun0 | grep inet | grep 10. | tr -s " " | cut -d " " -f 3 | cut -d "/" -f 1)
+lab=$(cat /etc/openvpn/*.conf | grep "remote " | cut -d " " -f 2 | cut -d "." -f 1 | cut -d "-" -f 2-)
+
+if [[ $htbip == *"10."* ]]
+then
+   gwip=$(ip route show dev tun0 | awk '/via/ {print $3}' | sort -u)
+   ping=$(ping -c 1 $gwip -W 1 | sed '$!d;s|.*/\([0-9.]*\)/.*|\1|' | cut -c1-4)
+   echo "𝗩𝗣𝗡 - $htbip [$ping ms]"
+else
+   echo "No VPN"
+fi
